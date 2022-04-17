7f20e4349c360d283f17702c85086974b5c80899
Shahid Shah <shahshahid2123@gmail.com>
2022-04-17 15:57:31 +0530

    feat: added methods and tests for dynamic list

--- src/main/java/com/shahid/javadsa/DynamicList.java
+++ src/main/java/com/shahid/javadsa/DynamicList.java
@@ -1,14 +1,15 @@
 package com.shahid.javadsa;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.StringJoiner;
+
 public class DynamicList<T> {
     private T[] list;
-    private int capacity;
-    private int size;
+    private int back = -1;
 
     DynamicList() {
-        this.capacity = 10;
-        this.list = (T[]) new Object[this.capacity];
-        this.size = 0;
+        this.list = (T[]) new Object[10];
     }
 
     DynamicList(int capacity) {
@@ -16,8 +17,42 @@ public class DynamicList<T> {
             throw new IllegalArgumentException("Initial Capacity must be greater than zero.");
         }
 
-        this.capacity = capacity;
-        this.list = (T[])new Object[this.capacity];
-        this.size = 0;
+        this.list = (T[])new Object[capacity];
+    }
+
+    public void print() {
+        StringJoiner sj = new StringJoiner(", ","[ "," ]");
+        Arrays.stream(this.list).filter(Objects::nonNull).map(String::valueOf).forEach(sj::add);
+        System.out.println(sj.toString());
+    }
+
+    public void push(T element) {
+        this.resizeArrayIfSizeEquals(this.getCapacity(), this.getCapacity() * 2);
+        this.list[++this.back] = element;
+    }
+
+    public T pop() {
+        if(this.getSize() == 0) {
+            throw new ArrayIndexOutOfBoundsException("List is empty");
+        }
+        T element = this.list[this.back--];
+        this.resizeArrayIfSizeEquals(this.getCapacity() / 2, this.getCapacity() / 2);
+        return element;
+    }
+
+    public int getSize() {
+        return this.back + 1;
+    }
+
+    public int getCapacity() {
+        return this.list.length;
+    }
+
+    private void resizeArrayIfSizeEquals(int capacityToCheck, int newCapacity) {
+        if(this.getSize() == capacityToCheck) {
+            T[] temp = (T[]) new Object[newCapacity];
+            System.arraycopy(this.list, 0, temp, 0, capacityToCheck);
+            this.list = temp;
+        }
     }
 }
--- src/test/java/com/shahid/javadsa/DynamicListTest.java
+++ src/test/java/com/shahid/javadsa/DynamicListTest.java
@@ -1,20 +1,111 @@
 package com.shahid.javadsa;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 @DisplayName("Dynamic List Test")
 public class DynamicListTest {
 
-    @DisplayName("Should throw error if capacity is less than 1.")
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {-1,-10,0})
+    @DisplayName("Should throw error if capacity is less than 1.")
     public void testPositiveCapacity(final int capacity) {
         Assertions.assertThrows(IllegalArgumentException.class,() -> {
             DynamicList<Integer> dynamicList = new DynamicList<>(capacity);
         });
+    }
+
+    @Test
+    @DisplayName("Should print list.")
+    public void testIfListPrints() {
+        DynamicList<Integer> dynamicList = new DynamicList<>();
+        dynamicList.push(1);
+        dynamicList.push(2);
+        dynamicList.push(3);
+        dynamicList.push(4);
+        dynamicList.print();
+        Assertions.assertEquals(outContent.toString().trim(), "[ 1, 2, 3, 4 ]");
+    }
+
+    @Test
+    @DisplayName("Should increase capacity if list is full.")
+    public void testIfArrayGrows() {
+        DynamicList<Integer> dynamicList = new DynamicList<>(2);
+        dynamicList.push(1);
+        dynamicList.push(2);
+        dynamicList.push(3);
+        Assertions.assertEquals(dynamicList.getSize(), 3);
+        Assertions.assertEquals(dynamicList.getCapacity(), 4);
+    }
+
+    @Test
+    @DisplayName("Should return the element removed.")
+    public void testIfPopReturnsLastElement() {
+        DynamicList<Integer> dynamicList = new DynamicList<>(2);
+        dynamicList.push(1);
+        dynamicList.push(2);
+        Assertions.assertEquals(dynamicList.pop(), 2);
+        Assertions.assertEquals(dynamicList.pop(), 1);
+    }
+
+    @Test
+    @DisplayName("Should decrease capacity if list is half empty.")
+    public void testIfArrayShrinks() {
+        DynamicList<Integer> dynamicList = new DynamicList<>(2);
+        dynamicList.push(1);
+        dynamicList.push(2);
+        dynamicList.pop();
+        Assertions.assertEquals(dynamicList.getSize(), 1);
+        Assertions.assertEquals(dynamicList.getCapacity(), 1);
+    }
+
+    @Test
+    @DisplayName("Should throw error if no element in list to remove.")
+    public void testIfExceptionIsThrownOnPop() {
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class,() -> {
+            DynamicList<Integer> dynamicList = new DynamicList<>();
+            dynamicList.pop();
+        });
+
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class,() -> {
+            DynamicList<Integer> dynamicList = new DynamicList<>();
+            dynamicList.push(1);
+            dynamicList.pop();
+            dynamicList.pop();
+        });
+    }
+
+    @Test
+    @DisplayName("Should return correct list size and capacity.")
+    public void testListSizeAndCapacity() {
+        DynamicList<Integer> dynamicList = new DynamicList<>();
+        dynamicList.push(1);
+        Assertions.assertEquals(dynamicList.getSize(),1);
+        Assertions.assertEquals(dynamicList.getCapacity(),10);
+
+        dynamicList.push(2);
+        Assertions.assertEquals(dynamicList.getSize(),2);
+        Assertions.assertEquals(dynamicList.getCapacity(),10);
     }
 }
